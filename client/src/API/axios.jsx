2b72552ba7655df549de353288191b6d8ebc8cea
coin <ashutosh23102001@gmail.com>
2025-12-28 19:53:57 +0530

    remove all comments

--- client/src/API/axios.jsx
+++ client/src/API/axios.jsx
@@ -1,31 +1,3 @@
-// import axios from "axios";
-// const api = axios.create({
-//   baseURL: 'http://localhost:3002',
-//   withCredentials: true
-// });
-
-// export default api;
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:3002/api",
-//   withCredentials: true
-// });
-
-// export default api;
-// src/API/axios.js
-
-
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:3002/api",
-//   withCredentials: true
-// });
-
-// export default api;
 
 import axios from "axios";
 
