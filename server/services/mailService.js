0027c385f62188ff527d928bf35fc216df6f7057
coin <ashutosh23102001@gmail.com>
2026-08-06 11:54:24 +0530

    eoor render

--- server/services/mailService.js
+++ server/services/mailService.js
@@ -10,8 +10,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true,
-
+  secure: true, // SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -20,8 +19,6 @@ const transporter = nodemailer.createTransport({
   connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 30000,
-
-  family: 4, // Force IPv4
 });
 
 /* =========================================
